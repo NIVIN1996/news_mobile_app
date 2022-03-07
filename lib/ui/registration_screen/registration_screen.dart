9908b39e61a21b5cf11eb0ae2f0140e234f5a1e7
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-08 00:29:29 +0530

    registration screen

---
+++ lib/ui/registration_screen/registration_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/color/colors.dart';
+import 'package:news_mobile_app/utils/common_padding/common_padding.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+
+import '../../utils/text_style/text_style.dart';
+import '../../widgets/button_widget/custom_button_widget.dart';
+import '../../widgets/text_form_widget/text_form_widget.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.whiteColor,
+      body: Center(
+        child: Container(
+          height: context.percentHeight * 50,
+          width: double.infinity,
+          margin: EdgeInsets.all(context.widthPx * 20.0),
+          decoration: BoxDecoration(color: AppColor.whiteColor,
+              boxShadow: const [BoxShadow(color: AppColor.lightColor, blurRadius: 5.0)],
+              borderRadius: BorderRadius.circular(context.widthPx * 10), border: Border.all(color: AppColor.hintColor)),
+          child: Padding(
+            padding: CommonPadding.paddingW25(context),
+            child: Column(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Padding(
+                  padding: CommonPadding.paddingH10(context),
+                  child: Text("News4U",
+                      textAlign: TextAlign.right, style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 30)),
+                ),
+
+                TextFormWidget(
+                  labelText: "Email",
+                  controller: emailController,
+                ),
+                TextFormWidget(
+                  labelText: "Password",
+                  controller: passwordController,
+                ),
+                TextFormWidget(
+                  labelText: "Confirm Password",
+                  controller: passwordController,
+                ),
+                Padding(
+                  padding: CommonPadding.paddingH10(context),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Forgot Password?",
+                          textAlign: TextAlign.right, style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 14)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: CommonPadding.paddingH10(context),
+                  child: const CustomButton(label: "Sign In",),
+                ),
+                Padding(
+                  padding: CommonPadding.paddingH10(context),
+                  child: GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: Text("Don't have an Account? Sign Up",
+                        textAlign: TextAlign.right, style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 14)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

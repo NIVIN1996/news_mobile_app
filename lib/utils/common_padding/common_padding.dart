c0adbdd2e945683a700554af9ef24dcb50be35ee
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-08 00:36:01 +0530

    common padding config

---
+++ lib/utils/common_padding/common_padding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+
+class CommonPadding {
+  static EdgeInsetsGeometry paddingW25(BuildContext context) {
+    return EdgeInsets.symmetric(horizontal: context.widthPx * 25);
+  }
+  static EdgeInsetsGeometry paddingH10(BuildContext context) {
+    return EdgeInsets.symmetric( vertical: context.heightPx * 10);
+  }
+}

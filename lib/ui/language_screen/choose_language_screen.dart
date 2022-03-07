70c3ea8ae63c326db069936427132ddd6301dbd8
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-08 00:36:20 +0530

    language screen ui

---
+++ lib/ui/language_screen/choose_language_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+
+import '../../models/language_model/language_model.dart';
+import '../../utils/color/colors.dart';
+import '../../utils/text_style/text_style.dart';
+
+class ChooseLanguageScreen extends StatefulWidget {
+  const ChooseLanguageScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLanguageScreenState createState() => _ChooseLanguageScreenState();
+}
+
+class _ChooseLanguageScreenState extends State<ChooseLanguageScreen> {
+  int _value2 = 0;
+  final List<GroupModel> _group = [
+    GroupModel(text: "English", index: 1, selected: true),
+    GroupModel(text: "Malayalam", index: 2, selected: false),
+    GroupModel(text: "Tamil", index: 3, selected: false),
+    GroupModel(text: "Hindi", index: 4, selected: false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.whiteColor,
+      body: Center(
+        child: Container(
+          height: context.percentHeight * 40,
+          width: double.infinity,
+          margin: EdgeInsets.all(context.widthPx * 20.0),
+          decoration: BoxDecoration(color: AppColor.whiteColor,
+              boxShadow: const [BoxShadow(color: AppColor.lightColor, blurRadius: 5.0)],
+              borderRadius: BorderRadius.circular(context.widthPx * 10), border: Border.all(color: AppColor.hintColor)),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: context.heightPx * 10.0),
+                child: Text(
+                  'Choose Language',
+                  textAlign: TextAlign.center,
+                  style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20),
+                ),
+              ),
+              const Divider(
+                color: AppColor.grey3,
+                thickness: 0.5,
+              ),
+              Column(
+                children: <Widget>[makeRadioTileList()],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeRadioTileList() {
+    List<Widget> list = <Widget>[];
+    for (int i = 0; i < _group.length; i++) {
+      list.add(RadioListTile(
+        value: _group[i].index,
+        groupValue: _value2,
+        title: Text(
+          ' ${_group[i].text}',
+          style:
+              TextFontStyle.med(color: _group[i].selected ? AppColor.black : AppColor.grey3, size: context.textPx * 20),
+        ),
+        selected: _group[i].selected,
+        onChanged: (int? val) {
+          setState(() {
+            for (int i = 0; i < _group.length; i++) {
+              _group[i].selected = false;
+            }
+            _value2 = val!;
+            _group[i].selected = true;
+          });
+        },
+        activeColor: Colors.orangeAccent,
+        controlAffinity: ListTileControlAffinity.leading,
+      ));
+    }
+
+    Column column = Column(
+      children: list,
+    );
+    return column;
+  }
+}

8e8fba6379eb0a9f35b70dd6c8808baa5731bfd4
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-08 10:10:49 +0530

    named navigation for registration added

--- lib/services/navigator/routes.dart
+++ lib/services/navigator/routes.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:news_mobile_app/ui/login_screen/login_screen.dart';
 
 import '../../ui/home_screen/home_screen.dart';
 import '../../ui/language_screen/choose_language_screen.dart';
 import '../../ui/registration_screen/registration_screen.dart';
 
-
 class ScreenNames {
   static String currentRoute = "/";
-  static const String login = "ui/signup_screen/login_screen";
+  static const String login = "ui/login_screen/login_screen";
   static const String chooseLanguageScreen = "ui/signup_screen/chooseLanguageScreen";
   static const String homeScreen = "ui/home_screen/home_screen";
   static const String registrationScreen = "ui/registration_screen/registration_screen";
-
-
-
 }
 
 Route<dynamic> generateRoute(RouteSettings settings) {
@@ -21,12 +18,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
 
   switch (settings.name) {
     case ScreenNames.login:
-      return MaterialPageRoute(builder: (context) => const HomeScreen());
-    case ScreenNames.chooseLanguageScreen:
-      return MaterialPageRoute(builder: (context) => const ChooseLanguageScreen());
+      return MaterialPageRoute(builder: (context) => const LoginScreen());
     case ScreenNames.registrationScreen:
       return MaterialPageRoute(builder: (context) => const RegistrationScreen());
-
+    case ScreenNames.chooseLanguageScreen:
+      return MaterialPageRoute(builder: (context) => const ChooseLanguageScreen());
+    case ScreenNames.homeScreen:
+      return MaterialPageRoute(builder: (context) => const HomeScreen());
 
     default:
       return MaterialPageRoute(builder: (context) => const HomeScreen());

7d1bc534058c9843b1ca9eeb748cf44c093c7748
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-08 17:56:34 +0530

    Appbar widget updated

--- lib/widgets/app_bar_widget/app_bar_widget.dart
+++ lib/widgets/app_bar_widget/app_bar_widget.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
 
-class AppbarWidget extends StatelessWidget {
-  const AppbarWidget({Key? key}) : super(key: key);
+import '../../ui/menu_screen/menu_screen.dart';
+import '../../ui/menu_screen/menu_screen.dart';
+import '../../ui/menu_screen/menu_screen.dart';
+import '../../utils/color/colors.dart';
+import '../../utils/text_style/text_style.dart';
+import 'appbar_size/appbar_size.dart';
+
+class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final bool showFilter;
+  final String label;
+  final AppBar appBar;
+  final void Function()? menuPress;
+  final void Function()? notificationPress;
+
+  const AppBarWidget({
+    Key? key,
+    required this.appBar,
+    this.menuPress,
+    this.notificationPress,
+    this.showFilter = true,
+    this.label = "News4U",
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(AppBarSize.appBarHeight(appBar));
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppBar(
+      //! toolbar height
+      backgroundColor: AppColor.whiteColor,
+      elevation: 0,
+      title: Center(
+        child: Text(
+          "News4U",
+          style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 24),
+        ),
+      ),
+      //! leading padding
+      leading: Builder(builder: (context) {
+        return GestureDetector(
+          onTap: () {
+            Scaffold.of(context).openDrawer();
+          },
+          child: Container(padding: AppBarSize.leadingPadding(context), child: AppBarSize.menuIconWidget(context)),
+        );
+      }),
+      actions: [
+        Padding(
+          //! trailing padding
+          padding: AppBarSize.tailingPadding(context),
+          child: GestureDetector(
+            onTap: notificationPress ?? () {},
+            //! Search icon widget
+            child: Container(child: AppBarSize.notificationIconWidget(context)),
+          ),
+        ),
+      ],
+      centerTitle: true,
+    );
   }
 }
---
+++ lib/widgets/app_bar_widget/appbar_size/appbar_size.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/color/colors.dart';
+import '../../../utils/responsive_config/responsive_config.dart';
+
+
+class AppBarSize {
+  AppBarSize();
+
+  static double tabToolbarHeight = 100;
+  static double tabAppbarHeight = 100;
+
+  static double appBarHeight(AppBar appBar) {
+    double screenWidth = WindowProperties.screenWidth;
+    if (screenWidth > 600) {
+      return tabAppbarHeight;
+    } else {
+      return appBar.preferredSize.height;
+    }
+  }
+
+  static double toolbarHeight(AppBar appBar) {
+    double screenWidth = WindowProperties.screenWidth;
+    if (screenWidth > 600) {
+      return tabToolbarHeight;
+    } else {
+      return kToolbarHeight;
+    }
+  }
+
+  static double menuIconWidth(BuildContext context) {
+    return context.widthPx * 23;
+  }
+
+  static double notificationIconWidth(BuildContext context) {
+    return context.widthPx * 23;
+  }
+
+  static double leadingPaddingSize(BuildContext context) {
+    double pad = context.widthPx * 10;
+    if (context.screenWidth > 600) {
+      pad = 10;
+    }
+    return pad;
+  }
+  static EdgeInsets leadingPadding(BuildContext context) {
+    return  EdgeInsets.only(
+      left: AppBarSize.leadingPaddingSize(context),
+    );
+  }
+
+  static double tailingPaddingSize(BuildContext context) {
+    double pad = context.widthPx * 30;
+    if (context.screenWidth > 600) {
+      pad = 45;
+    }
+    return pad;
+  }
+
+  static EdgeInsets tailingPadding(BuildContext context) {
+    return  EdgeInsets.only(
+      right: AppBarSize.tailingPaddingSize(context),
+
+    );
+  }
+
+  static double iconScale() {
+    double screenWidth = WindowProperties.screenWidth;
+    if (screenWidth > 600) {
+      return 1.5;
+    } else {
+      return 1.0;
+    }
+  }
+
+  //! ** Icon Widgets
+
+  static Widget menuIconWidget(BuildContext context) {
+    return Container(
+      color: AppColor.whiteColor,
+      child: Transform.scale(
+        scale: iconScale(),
+        child: Image.asset(
+          "assets/icons/menu.png",
+
+          fit: BoxFit.scaleDown,
+          width: AppBarSize.menuIconWidth(context),
+        ),
+      ),
+    );
+  }
+
+  static Widget notificationIconWidget(BuildContext context) {
+    return Transform.scale(
+      scale: iconScale(),
+      child: Image.asset(
+        "assets/icons/notification.png",
+        fit: BoxFit.scaleDown,
+        //! Notification icon responsive height
+        width: AppBarSize.notificationIconWidth(context),
+      ),
+    );
+  }
+
+//! ** End Icons Widgets
+}

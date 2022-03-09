3ffdc5eddf185b7846a6994b3aed82db3a755b74
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-09 20:51:05 +0530

    padding added in news list item

--- lib/ui/home_screen/home_page_widget/news_list_item_widget.dart
+++ lib/ui/home_screen/home_page_widget/news_list_item_widget.dart
@@ -1,15 +1,19 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:news_mobile_app/services/navigator/routes.dart';
+import 'package:news_mobile_app/utils/navigation/navigation.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
 
 import '../../../utils/color/colors.dart';
 import '../../../utils/text_style/text_style.dart';
 
-
 class NewsListItemWidget extends StatefulWidget {
   final int index;
 
-  const NewsListItemWidget({Key? key, required this.index, }) : super(key: key);
+  const NewsListItemWidget({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
 
   @override
   _NewsListItemWidgetState createState() => _NewsListItemWidgetState();
@@ -21,9 +25,11 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        context.pushNamed(ScreenNames.newsDetailsScreen);
       },
       child: Card(
-        elevation: 5,shadowColor: AppColor.lightBlue,
+        elevation: 5,
+        shadowColor: AppColor.lightBlue,
         margin: EdgeInsets.symmetric(vertical: context.heightPx * 5, horizontal: context.widthPx * 25),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
@@ -37,7 +43,7 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
 
                 },
                 child: Padding(
-                  padding:  EdgeInsets.all(context.widthPx* 10.0),
+                  padding: EdgeInsets.all(context.widthPx * 10.0),
                   child: CachedNetworkImage(
                     imageUrl: "https://picsum.photos/200/300",
                     imageBuilder: (context, imageProvider) => Container(
@@ -66,26 +72,28 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
             Expanded(
               flex: 4,
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal:context.widthPx* 7),
+                padding: EdgeInsets.symmetric(horizontal: context.widthPx * 7),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(top: context.heightPx * 10.0,bottom: context.heightPx * 3.0),
+                      padding: EdgeInsets.only(top: context.heightPx * 10.0, bottom: context.heightPx * 3.0),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                           "Sports",
+                            "Sports",
                             textAlign: TextAlign.start,
                             style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 16),
                           ),
-                          GestureDetector(onTap: (){
-                            setState((){
-                              selectBookmark = !selectBookmark;
-                            });
-                          },
-                              child: Icon(selectBookmark ? Icons.bookmark : Icons.bookmark_outline,color: AppColor.yellow2, size:context.widthPx * 18)),
+                          GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectBookmark = !selectBookmark;
+                                });
+                              },
+                              child: Icon(selectBookmark ? Icons.bookmark : Icons.bookmark_outline,
+                                  color: AppColor.yellow2, size: context.widthPx * 18)),
                         ],
                       ),
                     ),
@@ -96,28 +104,26 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
                       overflow: TextOverflow.ellipsis,
                       style: TextFontStyle.regular(color: AppColor.black, size: context.textPx * 14),
                     ),
-
                     Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: context.heightPx * 8.0,),
-                      child: RichText(
-                        text:  TextSpan(
-                          children: [
-
-                             WidgetSpan(
-                              child: Padding(
-                                padding: EdgeInsets.only(right:context.widthPx * 5.0),
-                                child: Icon(Icons.access_time, size:context.widthPx * 18),
-                              ),
-                            ),
-                            TextSpan(
-                              text: "12/03/2022",
-                              style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
-                            ),
-                          ],
+                        padding: EdgeInsets.symmetric(
+                          vertical: context.heightPx * 8.0,
                         ),
-                      )
-                      ),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              WidgetSpan(
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: context.widthPx * 5.0),
+                                  child: Icon(Icons.access_time, size: context.widthPx * 18),
+                                ),
+                              ),
+                              TextSpan(
+                                text: "12/03/2022",
+                                style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
+                              ),
+                            ],
+                          ),
+                        )),
                   ],
                 ),
               ),

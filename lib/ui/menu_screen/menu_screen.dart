4ef925a268d2dc18cd16f133c2d92d01f43bf453
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-10 18:18:38 +0530

    color removed from menu screen

--- lib/ui/menu_screen/menu_screen.dart
+++ lib/ui/menu_screen/menu_screen.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
 
+import '../../providers/theme_provider/theme_provider.dart';
 import '../../utils/color/colors.dart';
 import '../../utils/text_style/text_style.dart';
+import 'package:provider/provider.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key}) : super(key: key);
@@ -15,81 +16,73 @@ class MenuScreen extends StatefulWidget {
 class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Drawer(
-
       child: ListView(
-
         padding: EdgeInsets.zero,
         children: [
-            DrawerHeader(
-             curve: Curves.easeIn,
+          DrawerHeader(
+            curve: Curves.easeIn,
             decoration: const BoxDecoration(
               color: AppColor.yellow,
             ),
-            child: Text('News4U',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 24)),
+            child: Text('News4U', style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 24)),
           ),
-
-           ExpansionTile(
-            title:  Text('Category',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20)),
+          ExpansionTile(
+            title: Text('Category', style: TextFontStyle.med(size: context.textPx * 20)),
             children: <Widget>[
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   ListTile(
-                    title:  Text('Politics',style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-
-                    },
+                    title: Text('Politics', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                    onTap: () {},
                   ),
                   ListTile(
-                    title:  Text('Business',style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-
-                    },
+                    title: Text('Business', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                    onTap: () {},
                   ),
                   ListTile(
-                    title:  Text('Sports',style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-
-                    },
+                    title: Text('Sports', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                    onTap: () {},
                   ),
                   ListTile(
-                    title:  Text('Entertainment',style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-
-                    },
+                    title: Text('Entertainment',
+                        style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                    onTap: () {},
                   ),
                 ],
               ),
-
             ],
           ),
-
           ListTile(
-            title:  Text('Profile Settings',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20)),
-            onTap: () {
-
-            },
-          ),
-
-          ListTile(
-            title:  Text('Dark Mode',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20)),
-            onTap: () {
-
-            },
+            title: Text('Profile Settings', style: TextFontStyle.med(size: context.textPx * 20)),
+            onTap: () {},
           ),
           ListTile(
-            title:  Text('Terms and Conditions',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20)),
-            onTap: () {
-
-            },
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark ? 'Dark Mode' : 'Light Mode',
+                    style: TextFontStyle.med(size: context.textPx * 20)),
+                Switch.adaptive(
+                    value: themeProvider.isDarkMode,
+                    onChanged: (value) {
+                      final provider = Provider.of<ThemeProvider>(context, listen: false);
+                      provider.toggleTheme(value);
+                    })
+              ],
+            ),
+            onTap: () {},
           ),
           ListTile(
-            title:  Text('Rate Us',style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 20)),
-            onTap: () {
-
-            },
+            title: Text('Terms and Conditions', style: TextFontStyle.med(size: context.textPx * 20)),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Rate Us', style: TextFontStyle.med(size: context.textPx * 20)),
+            onTap: () {},
           ),
         ],
       ),

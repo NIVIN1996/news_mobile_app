6d12f7f6b1791d1b067b0e869ab6f3e046f34844
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-10 18:19:19 +0530

    color removed from news details screen and minor changes

--- lib/ui/news_details_screen/news_details_screen.dart
+++ lib/ui/news_details_screen/news_details_screen.dart
@@ -1,8 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
-
 import '../../utils/color/colors.dart';
 import '../../utils/text_style/text_style.dart';
 
@@ -45,7 +43,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
               height: context.percentHeight * 50,
             ),
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal:context.widthPx* 20.0,vertical: context.heightPx*10),
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -55,12 +53,12 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                         WidgetSpan(
                           child: Padding(
                             padding: EdgeInsets.only(right: context.widthPx * 5.0),
-                            child: Icon(Icons.access_time,color: AppColor.grey5, size: context.widthPx * 18),
+                            child: Icon(Icons.access_time, color: AppColor.grey5, size: context.widthPx * 18),
                           ),
                         ),
                         TextSpan(
                           text: "Posted on 12/03/2022",
-                          style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 14),
+                          style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
                         ),
                       ],
                     ),
@@ -74,9 +72,8 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                             });
                           },
                           child: Padding(
-                            padding:  EdgeInsets.only(right:context.widthPx* 10.0),
-                            child: Icon(Icons.share,
-                                color: AppColor.grey5, size: context.widthPx * 30),
+                            padding: EdgeInsets.only(right: context.widthPx * 10.0),
+                            child: Icon(Icons.share, color: AppColor.grey5, size: context.widthPx * 30),
                           )),
                       GestureDetector(
                           onTap: () {
@@ -92,29 +89,27 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
               ),
             ),
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal:context.widthPx* 20.0,vertical: context.heightPx*10),
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Expanded(
-                    flex:3,
+                    flex: 3,
                     child: Text(
                       "IPL 2022: Rajasthan Royals team profile ",
                       textAlign: TextAlign.start,
-                      style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 25),
+                      style: TextFontStyle.med(size: context.textPx * 25),
                     ),
                   ),
-
-
                 ],
               ),
             ),
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal:context.widthPx* 20.0,vertical: context.heightPx*10),
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
               child: Text(
                 "Ravindra Jadeja returned to the No.1 spot in ICC Test Rankings for all-rounders after his historic all-round performance in India's massive win over Sri Lanka in the first of a 2-Test series in Mohali. Jadeja moved up two spots to replace Jason Holder at the top of the all-rounder chart while R Ashwin slipped to No. 3 from No. 2 despite his match-winning contribution in the Mohali Test.",
                 textAlign: TextAlign.justify,
-                style: TextFontStyle.regular(color: AppColor.black, size: context.textPx * 18),
+                style: TextFontStyle.regular(size: context.textPx * 18),
               ),
             ),
           ],

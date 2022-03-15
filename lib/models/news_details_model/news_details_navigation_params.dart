c309de6451c71d81184d3d673c7311c2c05afa85
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-15 16:14:26 +0530

    news details navigation params file

---
+++ lib/models/news_details_model/news_details_navigation_params.dart
@@ -0,0 +1,15 @@
+class NewsDetailsNavigationParameters {
+
+  String imageUrl;
+  String title;
+  String author;
+  String subTitle;
+  String publishedAt;
+  NewsDetailsNavigationParameters(
+      {required this.subTitle,
+
+      required this.title,
+      required this.publishedAt,
+      required this.imageUrl,
+      required this.author});
+}

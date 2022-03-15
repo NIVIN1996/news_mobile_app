76abc8eeef050f91b6ad70afef47b957e83aa9d0
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-15 16:14:55 +0530

    news details api integration

--- lib/ui/news_details_screen/news_details_screen.dart
+++ lib/ui/news_details_screen/news_details_screen.dart
@@ -1,11 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+import '../../models/news_details_model/news_details_navigation_params.dart';
 import '../../utils/color/colors.dart';
 import '../../utils/text_style/text_style.dart';
 
 class NewsDetailsScreen extends StatefulWidget {
-  const NewsDetailsScreen({Key? key}) : super(key: key);
+  final NewsDetailsNavigationParameters navigationParameters;
+  const NewsDetailsScreen(
+      {Key? key, required this.navigationParameters,
+    })
+      : super(key: key);
 
   @override
   _NewsDetailsScreenState createState() => _NewsDetailsScreenState();
@@ -22,7 +27,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CachedNetworkImage(
-              imageUrl: "https://picsum.photos/200/300",
+              imageUrl: widget.navigationParameters.imageUrl,
               imageBuilder: (context, imageProvider) => Container(
                 decoration: BoxDecoration(
                   image: DecorationImage(
@@ -35,7 +40,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(context.widthPx * 20),
                   image: const DecorationImage(
-                    image: AssetImage('assets/common/no_img.jpg'),
+                    image: AssetImage('assets/common/no_img.png'),
                     fit: BoxFit.fitWidth,
                   ),
                 ),
@@ -57,7 +62,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                           ),
                         ),
                         TextSpan(
-                          text: "Posted on 12/03/2022",
+                          text: "Posted on ${widget.navigationParameters.publishedAt}",
                           style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
                         ),
                       ],
@@ -92,7 +97,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                   Expanded(
                     flex: 3,
                     child: Text(
-                      "IPL 2022: Rajasthan Royals team profile ",
+                      widget.navigationParameters.title,
                       textAlign: TextAlign.start,
                       style: TextFontStyle.med(size: context.textPx * 25),
                     ),
@@ -103,7 +108,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
               child: Text(
-                "Ravindra Jadeja returned to the No.1 spot in ICC Test Rankings for all-rounders after his historic all-round performance in India's massive win over Sri Lanka in the first of a 2-Test series in Mohali. Jadeja moved up two spots to replace Jason Holder at the top of the all-rounder chart while R Ashwin slipped to No. 3 from No. 2 despite his match-winning contribution in the Mohali Test.",
+                widget.navigationParameters.subTitle,
                 textAlign: TextAlign.justify,
                 style: TextFontStyle.regular(size: context.textPx * 18),
               ),

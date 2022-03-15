3233cdaf24fbeb30ae227ad035d6b9a8a7b489f9
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-15 11:43:23 +0530

    category news listing api integration

--- lib/ui/home_screen/home_page_widget/category_widget.dart
+++ lib/ui/home_screen/home_page_widget/category_widget.dart
@@ -3,13 +3,41 @@ import 'package:news_mobile_app/services/navigator/routes.dart';
 import 'package:news_mobile_app/utils/navigation/navigation.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
 
+import '../../../models/category_model/category_model_navigation_params.dart';
+import '../../../models/top_news_headline_model/top_news_headline_model.dart';
+import '../../../services/api_services/news_list_services/news_list_services.dart';
 import '../../../utils/color/colors.dart';
+import '../../../utils/static/enums.dart';
 import 'category_item_widget.dart';
 
-class CategoryWidget extends StatelessWidget {
-  const CategoryWidget({
-    Key? key,
-  }) : super(key: key);
+class CategoryWidget extends StatefulWidget {
+  const CategoryWidget({Key? key}) : super(key: key);
+
+  @override
+  _CategoryWidgetState createState() => _CategoryWidgetState();
+}
+
+class _CategoryWidgetState extends State<CategoryWidget> {
+  TopHeadlineNewsModel? politicsListModel;
+  TopHeadlineNewsModel? businessListModel;
+  TopHeadlineNewsModel? sportsListModel;
+  TopHeadlineNewsModel? entertainmentListModel;
+  ApiStatus politicsListStatus = ApiStatus.none;
+  ApiStatus businessListStatus = ApiStatus.none;
+  ApiStatus sportsListStatus = ApiStatus.none;
+  ApiStatus entertainmentListStatus = ApiStatus.none;
+  String politicsListApiError = "";
+  String businessListApiError = "";
+  String sportsListApiError = "";
+  String entertainmentListApiError = "";
+  @override
+  void initState() {
+    _getBusinessNews();
+    _getPoliticsNews();
+    _getEntertainmentNews();
+    _getSportsNews();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +56,15 @@ class CategoryWidget extends StatelessWidget {
                 image: "assets/icons/politics.png",
                 gridColor: AppColor.green3,
                 onPress: () {
-                  context.pushNamed(ScreenNames.newsCategoryListScreen,arguments: "Politics");
+                  context.pushNamed(
+                    ScreenNames.newsCategoryListScreen,
+                    arguments: CategoryNewsNavigationParameters(
+                        category: "Politics",
+                        callBack: _getPoliticsNews,
+                        categoryListModel: politicsListModel,
+                        categoryNewsStatus: politicsListStatus,
+                        errorStatus: politicsListApiError),
+                  );
                 },
               ),
               SizedBox(
@@ -40,7 +76,15 @@ class CategoryWidget extends StatelessWidget {
                 image: "assets/icons/stats.png",
                 gridColor: AppColor.orange,
                 onPress: () {
-                  context.pushNamed(ScreenNames.newsCategoryListScreen,arguments: "Business");
+                  context.pushNamed(
+                    ScreenNames.newsCategoryListScreen,
+                    arguments: CategoryNewsNavigationParameters(
+                        category: "Business",
+                        callBack: _getBusinessNews,
+                        categoryListModel: businessListModel,
+                        categoryNewsStatus: businessListStatus,
+                        errorStatus: businessListApiError),
+                  );
                 },
               ),
             ],
@@ -57,7 +101,13 @@ class CategoryWidget extends StatelessWidget {
                 image: "assets/icons/sport.png",
                 gridColor: AppColor.primaryColor3,
                 onPress: () {
-                  context.pushNamed(ScreenNames.newsCategoryListScreen,arguments: "Sports");
+                  context.pushNamed(ScreenNames.newsCategoryListScreen,
+                      arguments: CategoryNewsNavigationParameters(
+                          category: "Sports",
+                          callBack: _getSportsNews,
+                          categoryListModel: sportsListModel,
+                          categoryNewsStatus: sportsListStatus,
+                          errorStatus: sportsListApiError),);
                 },
               ),
               SizedBox(
@@ -69,8 +119,15 @@ class CategoryWidget extends StatelessWidget {
                 image: "assets/icons/cinema.png",
                 gridColor: AppColor.yellow2,
                 onPress: () {
-                  context.pushNamed(ScreenNames.newsCategoryListScreen,arguments: "Entertainment");
-
+                  context.pushNamed(
+                    ScreenNames.newsCategoryListScreen,
+                    arguments: CategoryNewsNavigationParameters(
+                        category: "Entertainment",
+                        callBack: _getEntertainmentNews,
+                        categoryListModel: entertainmentListModel,
+                        categoryNewsStatus: entertainmentListStatus,
+                        errorStatus: entertainmentListApiError),
+                  );
                 },
               ),
             ],
@@ -81,5 +138,77 @@ class CategoryWidget extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  _getBusinessNews() async {
+    setState(() {
+      businessListApiError = "";
+      businessListStatus = ApiStatus.loading;
+    });
+
+    businessListModel = await NewsListingService.getBusinessNews();
+
+    setState(() {
+      if (businessListModel!.status == "ok") {
+        businessListStatus = ApiStatus.success;
+      } else {
+        businessListApiError = businessListModel!.status;
+        businessListStatus = ApiStatus.error;
+      }
+    });
+  }
+
+  _getPoliticsNews() async {
+    setState(() {
+      politicsListApiError = "";
+      politicsListStatus = ApiStatus.loading;
+    });
+
+    politicsListModel = await NewsListingService.getPolitics();
+
+    setState(() {
+      if (politicsListModel!.status == "ok") {
+        politicsListStatus = ApiStatus.success;
+      } else {
+        politicsListApiError = politicsListModel!.status;
+        politicsListStatus = ApiStatus.error;
+      }
+    });
+  }
+
+  _getSportsNews() async {
+    setState(() {
+      sportsListApiError = "";
+      sportsListStatus = ApiStatus.loading;
+    });
+
+    sportsListModel = await NewsListingService.getSports();
+
+    setState(() {
+      if (sportsListModel!.status == "ok") {
+        sportsListStatus = ApiStatus.success;
+      } else {
+        sportsListApiError = sportsListModel!.status;
+        sportsListStatus = ApiStatus.error;
+      }
+    });
+  }
+
+  _getEntertainmentNews() async {
+    setState(() {
+      entertainmentListApiError = "";
+      entertainmentListStatus = ApiStatus.loading;
+    });
+
+    entertainmentListModel = await NewsListingService.getEntertainment();
+
+    setState(() {
+      if (entertainmentListModel!.status == "ok") {
+        entertainmentListStatus = ApiStatus.success;
+      } else {
+        entertainmentListApiError = entertainmentListModel!.status;
+        entertainmentListStatus = ApiStatus.error;
+      }
+    });
   }
 }
--- lib/ui/news_category_list_screen/news_category_list_screen.dart
+++ lib/ui/news_category_list_screen/news_category_list_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../../models/category_model/category_model_navigation_params.dart';
+
 import '../../widgets/app_bar_widget/common_app_bar_widget.dart';
 import '../../widgets/popup_widget/popup_widget.dart';
 import '../home_screen/home_page_widget/news_list_widget.dart';
 
 class NewsCategoryListScreen extends StatefulWidget {
-  final String category;
-  const NewsCategoryListScreen({Key? key, required this.category}) : super(key: key);
+  final CategoryNewsNavigationParameters navigationParameters;
+  const NewsCategoryListScreen({
+    Key? key,
+    required this.navigationParameters,
+  }) : super(key: key);
 
   @override
   _NewsCategoryListScreenState createState() => _NewsCategoryListScreenState();
@@ -18,7 +23,7 @@ class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
     return Scaffold(
       appBar: CommonAppBarWidget(
         showFilter: true,
-        label: widget.category,
+        label: widget.navigationParameters.category,
         showNotification: true,
         filterPress: () {
           showDialog(context: context, builder: (BuildContext context) => const PopUpWidget());
@@ -31,8 +36,14 @@ class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: const [
-            // NewsListWidget(),
+          children: [
+            NewsListWidget(
+              status: widget.navigationParameters.categoryNewsStatus,
+              apiError: widget.navigationParameters.errorStatus,
+              model: widget.navigationParameters.categoryListModel,
+              isDelivered: true,
+              retryCallBack: widget.navigationParameters.callBack,
+            ),
           ],
         ),
       ),

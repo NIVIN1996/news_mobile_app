f2e6e1d0335d763b86fbc8bfa5e461df2666f5cb
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-17 15:57:08 +0530

    multiProvider

---
+++ lib/providers/common_function_provider/provider_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+
+
+
+class ProviderRegistrar {
+  static List<SingleChildWidget> providers = [
+    // ChangeNotifierProvider(create: (_) => NewsSearchProvider()),
+
+  ];
+
+}

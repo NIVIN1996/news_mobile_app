1d9dc0ef0979938f3fb9a22d1a42278ecf6163a8
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-17 15:40:57 +0530

    search news service and endpoint

--- lib/services/end_points/end_points.dart
+++ lib/services/end_points/end_points.dart
@@ -1,10 +1,15 @@
 class NewsAppUrls {
   static const baseUrl = 'https://newsapi.org/v2/';
-  static const apiKey = 'apiKey=1273ef6fa8e64ea19ea60df0ebca1353';
+  static const apiKey = 'apiKey=2e6c65e5132041429e549af8c6bf8c56';
 
   static const topNewsHeadlines = baseUrl + 'top-headlines?country=in&' + apiKey;
   static const businessNewsHeadlines = baseUrl + 'top-headlines?country=in&category=business&' + apiKey;
   static const sportsNewsHeadlines = baseUrl + 'top-headlines?country=in&category=sports&' + apiKey;
   static const entertainmentNewsHeadlines = baseUrl + 'top-headlines?country=in&category=entertainment&' + apiKey;
   static const politicsNewsHeadlines = baseUrl + 'top-headlines?country=in&category=politics&' + apiKey;
+  static const usNewsHeadlines = baseUrl + 'top-headlines?country=us&' + apiKey;
+  static const japanNewsHeadlines = baseUrl + 'top-headlines?country=jp&' + apiKey;
+  static const indiaNewsHeadlines = baseUrl + 'top-headlines?country=in&' + apiKey;
+  static const russiaNewsHeadlines = baseUrl + 'top-headlines?country=ru&' + apiKey;
+  static const searchNewsHeadlines = baseUrl + 'everything?';
 }

deb79ae18157e44cc0afaee39a30bc70a4c39d94
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-18 12:27:09 +0530

    removed filter from appbar widget

--- lib/ui/news_category_list_screen/news_category_list_screen.dart
+++ lib/ui/news_category_list_screen/news_category_list_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:news_mobile_app/ui/widgets/popup_widget/popup_widget.dart';
 
 import '../../models/category_model/category_model_navigation_params.dart';
 
-import '../../widgets/app_bar_widget/common_app_bar_widget.dart';
-import '../../widgets/popup_widget/popup_widget.dart';
 import '../home_screen/home_page_widget/news_list_widget/news_list_widget.dart';
+import '../widgets/app_bar_widget/common_app_bar_widget.dart';
 
 class NewsCategoryListScreen extends StatefulWidget {
   final CategoryNewsNavigationParameters navigationParameters;
@@ -22,12 +22,9 @@ class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CommonAppBarWidget(
-        showFilter: true,
         label: widget.navigationParameters.category,
         showNotification: true,
-        filterPress: () {
-          showDialog(context: context, builder: (BuildContext context) => const PopUpWidget());
-        },
+
         backPress: () {
           Navigator.pop(context);
         },

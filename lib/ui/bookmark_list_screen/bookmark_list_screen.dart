9178e56cd1ef5207784164fb5c745da8a60b9402
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-19 13:09:07 +0530

    bookmark screen for listing

---
+++ lib/ui/bookmark_list_screen/bookmark_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookmarkListScreen extends StatefulWidget {
+  const BookmarkListScreen({Key? key}) : super(key: key);
+
+  @override
+  _BookmarkListScreenState createState() => _BookmarkListScreenState();
+}
+
+class _BookmarkListScreenState extends State<BookmarkListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

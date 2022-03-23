3de1755dd7e8befe8d02618a8aa7192a24c5131d
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-23 22:41:31 +0530

    category url changed in endpoints

--- lib/services/end_points/end_points.dart
+++ lib/services/end_points/end_points.dart
@@ -1,11 +1,12 @@
 class NewsAppUrls {
   static const baseUrl = 'https://newsapi.org/v2/';
-  static const apiKey = 'apiKey=2e6c65e5132041429e549af8c6bf8c56';
+  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
 
   static const topNewsHeadlines = baseUrl + 'top-headlines?country=in&' + apiKey;
   static const businessNewsHeadlines = baseUrl + 'top-headlines?country=in&category=business&' + apiKey;
+  static const newsHeadlines = baseUrl + 'top-headlines?country=in&';
   static const sportsNewsHeadlines = baseUrl + 'top-headlines?country=in&category=sports&' + apiKey;
-  static const entertainmentNewsHeadlines = baseUrl + 'top-headlines?country=in&category=entertainment&' + apiKey;
+  static const entertainmentNewsHeadlines = baseUrl + 'top-headlines?country=in&category=Entertainment&' + apiKey;
   static const politicsNewsHeadlines = baseUrl + 'top-headlines?country=in&category=politics&' + apiKey;
   static const indiaNewsHeadlines = baseUrl + 'top-headlines?';
   static const searchNewsHeadlines = baseUrl + 'everything?';

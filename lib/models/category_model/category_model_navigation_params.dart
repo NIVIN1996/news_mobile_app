04c9c592b1e055d695b504318f341bf8064e1981
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-23 22:14:32 +0530

    unwanted navigation params model removed

--- lib/models/category_model/category_model_navigation_params.dart
+++ lib/models/category_model/category_model_navigation_params.dart
@@ -3,14 +3,9 @@ import '../top_news_headline_model/top_news_headline_model.dart';
 
 class CategoryNewsNavigationParameters {
   String category;
-  TopHeadlineNewsModel? categoryListModel;
-  ApiStatus categoryNewsStatus;
-  String errorStatus;
-  Function callBack;
+
   CategoryNewsNavigationParameters(
       {required this.category,
-      required this.categoryListModel,
-      required this.categoryNewsStatus,
-      required this.errorStatus,
-      required this.callBack});
+
+      });
 }

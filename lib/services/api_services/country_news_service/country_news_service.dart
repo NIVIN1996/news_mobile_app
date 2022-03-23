33212ed01f737821fbdfc030c659fcbcf7cd1a6f
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-23 22:29:10 +0530

    api key changes in the services

--- lib/services/api_services/country_news_service/country_news_service.dart
+++ lib/services/api_services/country_news_service/country_news_service.dart
@@ -4,7 +4,7 @@ import 'package:news_mobile_app/services/end_points/end_points.dart';
 import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class CountryNewsService {
-  static const apiKey = 'apiKey=2e6c65e5132041429e549af8c6bf8c56';
+  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
   static Future<TopHeadlineNewsModel> getIndiaNewsHeadlines(String countryCode) async {
     return await ApiManager(url: NewsAppUrls.indiaNewsHeadlines+"country=$countryCode&"+apiKey).get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
--- lib/services/api_services/news_list_services/news_list_services.dart
+++ lib/services/api_services/news_list_services/news_list_services.dart
@@ -4,8 +4,15 @@ import 'package:news_mobile_app/services/end_points/end_points.dart';
 import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class NewsListingService {
+  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
   static Future<TopHeadlineNewsModel> getTopNewsHeadlines() async {
     return await ApiManager(url: NewsAppUrls.topNewsHeadlines).get((p0) {
+      return TopHeadlineNewsModel.fromJson(p0);
+    });
+  }
+
+  static Future<TopHeadlineNewsModel> getCategoryNews(String category) async {
+    return await ApiManager(url: NewsAppUrls.newsHeadlines+"category=$category&"+apiKey).get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
     });
   }

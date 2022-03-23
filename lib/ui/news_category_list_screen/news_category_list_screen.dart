bd3a771f4816f4548c316037dcefa07fdab11587
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-23 22:54:55 +0530

    unwanted import removed and provider used to get data

--- lib/ui/news_category_list_screen/news_category_list_screen.dart
+++ lib/ui/news_category_list_screen/news_category_list_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:news_mobile_app/ui/widgets/popup_widget/popup_widget.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+import 'package:provider/provider.dart';
+
 
 import '../../models/category_model/category_model_navigation_params.dart';
 
-import '../home_screen/home_page_widget/news_list_widget/news_list_widget.dart';
+import '../../providers/news_list_provider/news_list_provider.dart';
+import '../../utils/static/enums.dart';
+import '../../utils/text_style/text_style.dart';
+import '../home_screen/home_page_widget/news_list_widget/news_list_item_widget.dart';
+
 import '../widgets/app_bar_widget/common_app_bar_widget.dart';
+import '../widgets/shimmer_widget/shimmer_widget.dart';
 
 class NewsCategoryListScreen extends StatefulWidget {
   final CategoryNewsNavigationParameters navigationParameters;
@@ -19,12 +26,18 @@ class NewsCategoryListScreen extends StatefulWidget {
 
 class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      context.read<ArticleListProvider>().getCategoryArticle(widget.navigationParameters.category);
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CommonAppBarWidget(
         label: widget.navigationParameters.category,
         showNotification: true,
-
         backPress: () {
           Navigator.pop(context);
         },
@@ -33,14 +46,64 @@ class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            NewsListWidget(
-              status: widget.navigationParameters.categoryNewsStatus,
-              apiError: widget.navigationParameters.errorStatus,
-              model: widget.navigationParameters.categoryListModel,
-              isDelivered: true,
-              retryCallBack: widget.navigationParameters.callBack,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 25.0, vertical: context.heightPx * 10),
+              child: Text(
+                "Highlights",
+                textAlign: TextAlign.start,
+                style: TextFontStyle.semiBold(size: context.textPx * 20),
+              ),
             ),
+            Consumer<ArticleListProvider>(builder: (context, provider, _) {
+              if (provider.articleInitStatus == ApiStatus.loading) {
+                return Column(
+                  children: [
+                    for (int i = 0; i < 4; i++)
+                      Padding(
+                        padding:
+                        EdgeInsets.symmetric(horizontal: context.widthPx * 25.0, vertical: context.heightPx * 10),
+                        child: ShimmerWidget(
+                          height: context.heightPx * 140,
+                          width: double.infinity,
+                          radius: 10,
+                        ),
+                      )
+                  ],
+                );
+              }
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: context.heightPx * 5.0),
+                child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    itemCount: provider.articleInitStatus == ApiStatus.success
+                        ? provider.topHeadlineNewsModel!.articles.length
+                        : 5,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return provider.articleInitStatus == ApiStatus.success
+                          ? NewsListItemWidget(
+                        title: provider.topHeadlineNewsModel!.articles[index].title,
+                        imageUrl: provider.topHeadlineNewsModel!.articles[index].urlToImage,
+                        publishedAt: provider.topHeadlineNewsModel!.articles[index].publishedAt!,
+                        subTitle: provider.topHeadlineNewsModel!.articles[index].content,
+                        index: index,
+                        author: provider.topHeadlineNewsModel!.articles[index].source.name,
+                      )
+                          : Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: context.widthPx * 25.0, vertical: context.heightPx * 10),
+                        child: ShimmerWidget(
+                          height: context.heightPx * 140,
+                          width: context.widthPx * 100,
+                          radius: 10,
+                        ),
+                      );
+                    }),
+              );
+            }),
           ],
         ),
       ),

8724bf7373480096d0ef6d81206ce728a451be48
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-23 21:57:07 +0530

    hive field added to the article model

--- lib/models/top_news_headline_model/article_model.dart
+++ lib/models/top_news_headline_model/article_model.dart
@@ -1,34 +1,45 @@
+import 'package:hive/hive.dart';
 import 'package:news_mobile_app/models/top_news_headline_model/top_news_headline_model.dart';
+part 'article_model.g.dart';
 
+@HiveType(typeId: 0)
 class Article {
+  @HiveField(0)
+  final Source source;
+  @HiveField(1)
+  final String author;
+  @HiveField(2)
+  final String title;
+  @HiveField(3)
+  final String description;
+  @HiveField(4)
+  final String url;
+  @HiveField(5)
+  final String urlToImage;
+  @HiveField(6)
+  final DateTime? publishedAt;
+  @HiveField(7)
+  final String content;
+
   Article({
     required this.source,
-    required this.author,
-    required this.title,
-    required this.description,
-    required this.url,
-    required this.urlToImage,
-    required this.publishedAt,
-    required this.content,
+     required this.author,
+     required this.title,
+     required this.description,
+     required this.url,
+     required this.urlToImage,
+     this.publishedAt,
+     required this.content,
   });
 
-  Source source;
-  String author;
-  String title;
-  String description;
-  String url;
-  String urlToImage;
-  DateTime publishedAt;
-  String content;
-
   factory Article.fromJson(Map<String, dynamic> json) => Article(
-    source: Source.fromJson(json["source"]),
-    author: (json["author"] == null) ? "" : json["author"],
-    title: json["title"] ?? "",
-    description: json["description"] ?? "",
-    url: json["url"] ?? "",
-    urlToImage: json["urlToImage"] ?? "",
-    publishedAt: DateTime.parse(json["publishedAt"]),
-    content: (json["content"] == null) ? "" : json["content"],
-  );
+        source:json["source"] ==null? Source(id:"",name: ""): Source.fromJson(json["source"]),
+        author: (json["author"] == null) ? "" : json["author"],
+        title: json["title"] ?? "",
+        description: json["description"] ?? "",
+        url: json["url"] ?? "",
+        urlToImage: json["urlToImage"] ?? "",
+        publishedAt: DateTime.parse(json["publishedAt"]),
+        content: (json["content"] == null) ? "" : json["content"],
+      );
 }

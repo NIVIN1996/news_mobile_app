048ead3de6057b48a2bd1456ba91413789d1865b
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-24 19:40:12 +0530

    service name changed to getCountryNewsHeadlines

--- lib/services/api_services/country_news_service/country_news_service.dart
+++ lib/services/api_services/country_news_service/country_news_service.dart
@@ -5,7 +5,7 @@ import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class CountryNewsService {
   static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
-  static Future<TopHeadlineNewsModel> getIndiaNewsHeadlines(String countryCode) async {
+  static Future<TopHeadlineNewsModel> getCountryNewsHeadlines(String countryCode) async {
     return await ApiManager(url: NewsAppUrls.indiaNewsHeadlines+"country=$countryCode&"+apiKey).get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
     });

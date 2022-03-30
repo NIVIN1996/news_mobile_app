f399b4ae3cb2dc4e481ba0b1608ff90b2a4f458b
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-03-30 20:59:04 +0530

    menu screen theme provider calling and theme functionality implemented

--- lib/ui/menu_screen/menu_screen.dart
+++ lib/ui/menu_screen/menu_screen.dart
@@ -11,7 +11,6 @@ import '../../utils/color/colors.dart';
 import '../../utils/text_style/text_style.dart';
 import 'package:provider/provider.dart';
 
-
 class MenuScreen extends StatefulWidget {
   final User? currentUser;
   const MenuScreen({Key? key, required this.currentUser}) : super(key: key);
@@ -30,204 +29,225 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            curve: Curves.easeIn,
-            decoration: const BoxDecoration(
-              color: AppColor.yellow,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('News4U', style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 24)),
-                const SizedBox(
-                  height: 30,
-                ),
-                Text(widget.currentUser != null ? widget.currentUser!.displayName.toString().toTitleCase() : "",
-                    style: TextFontStyle.normal(color: AppColor.buttonActive, size: context.textPx * 20)),
-                Text(widget.currentUser != null ? widget.currentUser!.email.toString() : "",
-                    style: TextFontStyle.regular(color: AppColor.black, size: context.textPx * 15)),
-              ],
-            ),
-          ),
-          ExpansionTile(
-            title: Text('Category', style: TextFontStyle.med(size: context.textPx * 20)),
-            children: <Widget>[
-              Column(
+    return Consumer(builder: (context, ThemeProvider themeNotifier, child) {
+      return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              curve: Curves.easeIn,
+              decoration: const BoxDecoration(
+                color: AppColor.yellow,
+              ),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  ListTile(
-                    title: Text('Politics', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCategoryListScreen,
-                        arguments: CategoryNewsNavigationParameters(
-                          category: "politics",
-                        ),
-                      );
-                    },
+                  Text('News4U', style: TextFontStyle.med(color: AppColor.black, size: context.textPx * 24)),
+                  const SizedBox(
+                    height: 30,
                   ),
-                  ListTile(
-                    title: Text('Business', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCategoryListScreen,
-                        arguments: CategoryNewsNavigationParameters(
-                          category: "Business",
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Sports', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCategoryListScreen,
-                        arguments: CategoryNewsNavigationParameters(
-                          category: "Sports",
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Entertainment',
-                        style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCategoryListScreen,
-                        arguments: CategoryNewsNavigationParameters(
-                          category: "Entertainment",
-                        ),
-                      );
-                    },
-                  ),
+                  Text(widget.currentUser != null ? widget.currentUser!.displayName.toString().toTitleCase() : "",
+                      style: TextFontStyle.normal(color: AppColor.buttonActive, size: context.textPx * 20)),
+                  Text(widget.currentUser != null ? widget.currentUser!.email.toString() : "",
+                      style: TextFontStyle.regular(color: AppColor.black, size: context.textPx * 15)),
                 ],
               ),
-            ],
-          ),
-          ExpansionTile(
-            title: Text('Country', style: TextFontStyle.med(size: context.textPx * 20)),
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  ListTile(
-                    title: Text('India', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCountryListScreen,
-                        arguments: CountryNewsNavigationParameters(
-                          category: "",
-                          countryCode: "in",
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text('US', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCountryListScreen,
-                        arguments: CountryNewsNavigationParameters(
-                          category: "",
-                          countryCode: "us",
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Japan', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCountryListScreen,
-                        arguments: CountryNewsNavigationParameters(
-                          category: "",
-                          countryCode: "jp",
-                        ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Russia', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
-                    onTap: () {
-                      context.pushNamed(
-                        ScreenNames.newsCountryListScreen,
-                        arguments: CountryNewsNavigationParameters(
-                          category: "",
-                          countryCode: "ru",
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
-          ListTile(
-            title: Text('Profile Settings', style: TextFontStyle.med(size: context.textPx * 20)),
-            onTap: () {
-              widget.currentUser == null
-                  ? context.pushNamed(ScreenNames.login)
-                  : context.pushNamed(ScreenNames.profileScreen, arguments: widget.currentUser);
-            },
-          ),
-          ListTile(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark ? 'Dark Mode' : 'Light Mode',
-                    style: TextFontStyle.med(size: context.textPx * 20)),
-                Switch.adaptive(
-                    value: themeProvider.isDarkMode,
-                    onChanged: (value) {
-                      final provider = Provider.of<ThemeProvider>(context, listen: false);
-                      provider.toggleTheme(value);
-                    })
-              ],
             ),
-            onTap: () {},
-          ),
-          ListTile(
-            title: Text('Terms and Conditions', style: TextFontStyle.med(size: context.textPx * 20)),
-            onTap: () {
-              context.pushNamed(ScreenNames.termsAndConditionScreen);
-            },
-          ),
-          ListTile(
-            title: Text('Rate Us', style: TextFontStyle.med(size: context.textPx * 20)),
-            onTap: () {
-              context.pushNamed(ScreenNames.rateUsScreen);
-            },
-          ),
-          ListTile(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Log out', style: TextFontStyle.med(size: context.textPx * 20)),
-                const Icon(
-                  Icons.logout,
-                  size: 20,
+            ExpansionTile(
+              title: Text('Category', style: TextFontStyle.med(size: context.textPx * 20)),
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title:
+                          Text('Politics', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCategoryListScreen,
+                          arguments: CategoryNewsNavigationParameters(
+                            category: "politics",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title:
+                          Text('Business', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCategoryListScreen,
+                          arguments: CategoryNewsNavigationParameters(
+                            category: "Business",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Sports', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCategoryListScreen,
+                          arguments: CategoryNewsNavigationParameters(
+                            category: "Sports",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Entertainment',
+                          style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCategoryListScreen,
+                          arguments: CategoryNewsNavigationParameters(
+                            category: "Entertainment",
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
-            onTap: () async {
-              setState(() {
-                _isSigningOut = true;
-              });
-              await FirebaseAuth.instance.signOut();
-              setState(() {
-                _isSigningOut = false;
-              });
-              context.pushNamedAndRemoveUntil(ScreenNames.login);
-            },
-          ),
-        ],
-      ),
-    );
+            ExpansionTile(
+              title: Text('Country', style: TextFontStyle.med(size: context.textPx * 20)),
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: Text('India', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCountryListScreen,
+                          arguments: CountryNewsNavigationParameters(
+                            category: "",
+                            countryCode: "in",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('US', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCountryListScreen,
+                          arguments: CountryNewsNavigationParameters(
+                            category: "",
+                            countryCode: "us",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Japan', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCountryListScreen,
+                          arguments: CountryNewsNavigationParameters(
+                            category: "",
+                            countryCode: "jp",
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Russia', style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 16)),
+                      onTap: () {
+                        context.pushNamed(
+                          ScreenNames.newsCountryListScreen,
+                          arguments: CountryNewsNavigationParameters(
+                            category: "",
+                            countryCode: "ru",
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ListTile(
+              title: Text('Profile Settings', style: TextFontStyle.med(size: context.textPx * 20)),
+              onTap: () {
+                widget.currentUser == null
+                    ? context.pushNamed(ScreenNames.login)
+                    : context.pushNamed(ScreenNames.profileScreen, arguments: widget.currentUser);
+              },
+            ),
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(themeProvider.darkTheme ? 'Dark Mode' : 'Light Mode',
+                      style: TextFontStyle.med(size: context.textPx * 20)),
+                  Switch.adaptive(
+                      value: themeProvider.darkTheme,
+                      onChanged: (value) async {
+                        setState(() {
+                          themeProvider.darkTheme = value;
+                        });
+                      }),
+                ],
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Terms and Conditions', style: TextFontStyle.med(size: context.textPx * 20)),
+              onTap: () {
+                context.pushNamed(ScreenNames.termsAndConditionScreen);
+              },
+            ),
+            ListTile(
+              title: Text('Rate Us', style: TextFontStyle.med(size: context.textPx * 20)),
+              onTap: () {
+                context.pushNamed(ScreenNames.rateUsScreen);
+              },
+            ),
+            widget.currentUser == null
+                ? ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Sign In', style: TextFontStyle.med(size: context.textPx * 20)),
+                        const Icon(
+                          Icons.login,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      context.pushNamedAndRemoveUntil(ScreenNames.login);
+                    },
+                  )
+                : ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Sign out', style: TextFontStyle.med(size: context.textPx * 20)),
+                        const Icon(
+                          Icons.logout,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                    onTap: () async {
+                      setState(() {
+                        _isSigningOut = true;
+                      });
+                      await FirebaseAuth.instance.signOut();
+                      setState(() {
+                        _isSigningOut = false;
+                      });
+                      context.pushNamedAndRemoveUntil(ScreenNames.login);
+                    },
+                  ),
+          ],
+        ),
+      );
+    });
   }
 }

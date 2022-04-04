29749e19ecc315ba3db2c1d2bc5c9c443be22662
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-04 09:54:32 +0530

    bookmark_details_screen for bookmarked news details

---
+++ lib/ui/bookmark_list_screen/bookmark_details_screen.dart
@@ -0,0 +1,129 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
+
+import '../../models/news_details_model/news_details_navigation_params.dart';
+
+import '../../utils/color/colors.dart';
+import '../../utils/text_style/text_style.dart';
+
+class BookmarkNewsDetailsScreen extends StatefulWidget {
+  final NewsDetailsNavigationParameters navigationParameters;
+  const BookmarkNewsDetailsScreen({
+    Key? key,
+    required this.navigationParameters,
+  }) : super(key: key);
+
+  @override
+  _BookmarkNewsDetailsScreenState createState() => _BookmarkNewsDetailsScreenState();
+}
+
+class _BookmarkNewsDetailsScreenState extends State<BookmarkNewsDetailsScreen> {
+  bool selectBookmark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CachedNetworkImage(
+              imageUrl: widget.navigationParameters.imageUrl,
+              imageBuilder: (context, imageProvider) => Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              errorWidget: (context, url, error) => Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(context.widthPx * 20),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/common/no_img.png'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+              height: context.percentHeight * 50,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        WidgetSpan(
+                          child: Padding(
+                            padding: EdgeInsets.only(right: context.widthPx * 5.0),
+                            child: Icon(Icons.access_time, color: AppColor.grey5, size: context.widthPx * 18),
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Posted on ${widget.navigationParameters.publishedAt}",
+                          style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Row(
+                  //   children: [
+                  //     GestureDetector(
+                  //         onTap: () {},
+                  //         child: Padding(
+                  //           padding: EdgeInsets.only(right: context.widthPx * 10.0),
+                  //           child: Icon(Icons.share, color: AppColor.grey5, size: context.widthPx * 30),
+                  //         )),
+                  //     IconButton(
+                  //       icon: Icon(myList.contains(article) ? Icons.bookmark : Icons.bookmark_outline,
+                  //           color: AppColor.yellow2, size: context.widthPx * 30),
+                  //       onPressed: () {
+                  //         final article = selectedArticle[widget.navigationParameters.index];
+                  //
+                  //         if (!myList.contains(article)) {
+                  //           context.read<ArticleListProvider>().addBookmark(article);
+                  //         } else {
+                  //           context.read<ArticleListProvider>().removeFromList(article);
+                  //         }
+                  //       },
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      widget.navigationParameters.title,
+                      textAlign: TextAlign.start,
+                      style: TextFontStyle.med(size: context.textPx * 25),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 20.0, vertical: context.heightPx * 10),
+              child: Text(
+                widget.navigationParameters.subTitle,
+                textAlign: TextAlign.justify,
+                style: TextFontStyle.regular(size: context.textPx * 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

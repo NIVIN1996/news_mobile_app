1c75bfbf04b3cd36af57eea4b68e17c9d8eb8a92
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-04 10:01:06 +0530

    login page functionality minor changes and unwanted line of code removed

--- lib/ui/login_screen/login_screen.dart
+++ lib/ui/login_screen/login_screen.dart
@@ -30,6 +30,9 @@ class _LoginScreenState extends State<LoginScreen> {
   final _focusEmail = FocusNode();
   final _focusPassword = FocusNode();
 
+  bool showEmailUserNameValidation = false;
+  bool showPasswordValidation = false;
+
   bool _isProcessing = false;
 
   Future<FirebaseApp> _initializeFirebase() async {
@@ -38,17 +41,12 @@ class _LoginScreenState extends State<LoginScreen> {
     User? user = FirebaseAuth.instance.currentUser;
 
     if (user != null) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => HomeScreen(
-            user: user,
-          ),
-        ),
-      );
+      context.pushNamedAndRemoveUntil(ScreenNames.homeScreen,arguments: user);
     }
 
     return firebaseApp;
   }
+
   @override
   void initState() {
     _initializeFirebase();
@@ -58,143 +56,144 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Column(
-          children: [
-            Expanded(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Expanded(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: context.percentHeight * 50,
-                    width: double.infinity,
-                    margin: EdgeInsets.all(context.widthPx * 20.0),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).backgroundColor,
-                        boxShadow: const [BoxShadow(blurRadius: 3.0)],
-                        borderRadius: BorderRadius.circular(context.widthPx * 10),
-                        border: Border.all(color: AppColor.hintColor)),
-                    child: Form(
-                      key: _formKey,
-                      child: Padding(
-                        padding: CommonPadding.paddingW25(context),
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: CommonPadding.paddingH10(context),
-                              child: Text("News4U",
-                                  textAlign: TextAlign.right, style: TextFontStyle.med(size: context.textPx * 30)),
-                            ),
-                            TextFormWidget(
-                              labelText: "Email",
-                              controller: _emailTextController,
-                              focusNode: _focusEmail,
-                              // keyboardType: TextInputType.emailAddress,
-                              validation: (value) => Validator.validateEmail(
-                                email: value,
-                              ),
-                            ),
-                            TextFormWidget(
-                              labelText: "Password",
-                              showEyeIcon: true,
-                              controller: _passwordTextController,
-                              focusNode: _focusPassword,
-                              // keyboardType: TextInputType.text,
-                              validation: (value) => Validator.validatePassword(
-                                password: value,
-                              ),
-                            ),
-                            Padding(
-                              padding: CommonPadding.paddingH10(context),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  GestureDetector(
-                                    onTap: () {
-                                      context.pushNamed(ScreenNames.forgotPasswordScreen);
-                                    },
-                                    child: Text("Forgot Password?",
-                                        textAlign: TextAlign.right,
-                                        style: TextFontStyle.med(size: context.textPx * 14)),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            _isProcessing
-                                ? const CircularProgressIndicator()
-                                : Padding(
-                              padding: CommonPadding.paddingH10(context),
-                              child: CustomButton(
-                                label: "Sign In",
-                                onPress: () async {
-                                  _focusEmail.unfocus();
-                                  _focusPassword.unfocus();
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                // height: context.percentHeight * 55,
+                width: double.infinity,
+                margin: EdgeInsets.all(context.widthPx * 20.0),
+                decoration: BoxDecoration(
+                    color: Theme.of(context).backgroundColor,
+                    boxShadow: const [BoxShadow(blurRadius: 3.0)],
+                    borderRadius: BorderRadius.circular(context.widthPx * 10),
+                    border: Border.all(color: AppColor.hintColor)),
+                child: Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: CommonPadding.paddingW25(context),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: CommonPadding.paddingH10(context),
+                          child: Text("News4U",
+                              textAlign: TextAlign.right, style: TextFontStyle.med(size: context.textPx * 30)),
+                        ),
+                        TextFormWidget(
+                          labelText: "Email",
+                          controller: _emailTextController,
+                          focusNode: _focusEmail,
+                          maxLength: 70,
+                          // keyboardType: TextInputType.emailAddress,
+                          validation: (value) => Validator.validateEmail(
+                            email: value,
+                          ),
+                          onChange: (String text) {
+                            setState(() {
+                              showEmailUserNameValidation = false;
+                            });
+                          },
+                        ),
+                        TextFormWidget(
+                          labelText: "Password",
+                          showEyeIcon: true,
+                          controller: _passwordTextController,
+                          focusNode: _focusPassword,
+                          maxLength: 20,
+                          // keyboardType: TextInputType.text,
 
-                                  if (_formKey.currentState!.validate()) {
-                                    setState(() {
-                                      _isProcessing = true;
-                                    });
-
-                                    User? user = await FireAuth.signInUsingEmailPassword(
-                                      context: context,
-                                      email: _emailTextController.text,
-                                      password: _passwordTextController.text,
-                                    );
-
-                                    AppSnackBar.showSnackBarWithText(
-                                        context: context, text: "Logged In Successfully...");
-                                    setState(() {
-                                      _isProcessing = false;
-                                    });
-
-                                    if (user != null) {
-                                      context.pushNamed(ScreenNames.homeScreen, arguments: user);
-                                    } else {
-                                      AppSnackBar.showSnackBarWithText(
-                                          context: context,
-                                          text: "Login Failed",
-                                          backgroundColor: AppColor.red);
-                                    }
-                                  }
-                                },
-                              ),
-                            ),
-                            Padding(
-                              padding: CommonPadding.paddingH10(context),
-                              child: GestureDetector(
+                          validation: (value) => Validator.validatePassword(
+                            password: value,
+                          ),
+                          onChange: (String text) {
+                            setState(() {
+                              showPasswordValidation = false;
+                            });
+                          },
+                        ),
+                        Padding(
+                          padding: CommonPadding.paddingH10(context),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              GestureDetector(
                                 onTap: () {
-                                  context.pushNamed(ScreenNames.registrationScreen);
+                                  context.pushNamed(ScreenNames.forgotPasswordScreen);
                                 },
-                                child: Text("Don't have an Account? Sign Up",
+                                child: Text("Forgot Password?",
                                     textAlign: TextAlign.right, style: TextFontStyle.med(size: context.textPx * 14)),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
+                        _isProcessing
+                            ? const CircularProgressIndicator()
+                            : Padding(
+                                padding: CommonPadding.paddingH10(context),
+                                child: CustomButton(
+                                  label: "Sign In",
+                                  onPress: () async {
+                                    _focusEmail.unfocus();
+                                    _focusPassword.unfocus();
+                                    FocusManager.instance.primaryFocus?.unfocus();
+                                    if (_formKey.currentState!.validate()) {
+                                      // setState(() {
+                                      //   _isProcessing = true;
+                                      // });
+
+                                      User? user = await FireAuth.signInUsingEmailPassword(
+                                        context: context,
+                                        email: _emailTextController.text,
+                                        password: _passwordTextController.text,
+                                      );
+
+                                      // setState(() {
+                                      //   _isProcessing = false;
+                                      // });
+
+                                    }
+                                  },
+                                ),
+                              ),
+                        Padding(
+                          padding: CommonPadding.paddingH10(context),
+                          child: GestureDetector(
+                            onTap: () {
+                              context.pushNamed(ScreenNames.registrationScreen);
+                            },
+                            child: Text("Don't have an Account? Sign Up",
+                                textAlign: TextAlign.right, style: TextFontStyle.med(size: context.textPx * 14)),
+                          ),
+                        ),
+                      ],
                     ),
-                  )
-                ],
+                  ),
+                ),
               )
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextButton(
-                  onPressed: () {
-                    context.pushNamedReplacement(ScreenNames.homeScreen, arguments: null);
-                  },
-                  child:  Text(
-                          'Skip',
-                          textAlign: TextAlign.right,
-                          style: TextFontStyle.med(size: context.textPx * 20),
-                        ),
+            ],
+          )),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  context.pushNamedReplacement(ScreenNames.homeScreen, arguments: null);
+                },
+                child: Text(
+                  'Skip',
+                  textAlign: TextAlign.right,
+                  style: TextFontStyle.med(size: context.textPx * 20),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

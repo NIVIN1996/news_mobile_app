3cf7e9d02025a9e688bf0ccda5ad93eeb376a713
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-06 15:20:43 +0530

    commented unwanted sample test file

--- lib/providers/sample_provider.dart
+++ lib/providers/sample_provider.dart
@@ -1,41 +1,41 @@
-// provider/movie_provider.dart
-import 'package:flutter/material.dart';
-import 'dart:math';
-
-import '../models/sample_model/sample_model.dart';
-
-
-// A list of movies
-final List<Movie> initialData = List.generate(
-    10,
-        (index) => Movie(
-        title: "Movie $index",
-        runtime: "${Random().nextInt(100) + 60} minutes"));
-
-class MovieProvider with ChangeNotifier {
-  // All movies (that will be displayed on the Home screen)
-  final List<Movie> _movies = initialData;
-
-
-  // Retrieve all movies
-  List<Movie> get movies => _movies;
-
-  // Favorite movies (that will be shown on the MyList screen)
-  final List<Movie> _myList = [];
-
-  // Retrieve favorite movies
-  List<Movie> get myList => _myList;
-
-  // Adding a movie to the favorites list
-  void addToList(Movie movie) {
-    print(_movies);
-    _myList.add(movie);
-    notifyListeners();
-  }
-
-  // Removing a movie from the favorites list
-  void removeFromList(Movie movie) {
-    _myList.remove(movie);
-    notifyListeners();
-  }
-}
+// // provider/movie_provider.dart
+// import 'package:flutter/material.dart';
+// import 'dart:math';
+//
+// import '../models/sample_model/sample_model.dart';
+//
+//
+// // A list of movies
+// final List<Movie> initialData = List.generate(
+//     10,
+//         (index) => Movie(
+//         title: "Movie $index",
+//         runtime: "${Random().nextInt(100) + 60} minutes"));
+//
+// class MovieProvider with ChangeNotifier {
+//   // All movies (that will be displayed on the Home screen)
+//   final List<Movie> _movies = initialData;
+//
+//
+//   // Retrieve all movies
+//   List<Movie> get movies => _movies;
+//
+//   // Favorite movies (that will be shown on the MyList screen)
+//   final List<Movie> _myList = [];
+//
+//   // Retrieve favorite movies
+//   List<Movie> get myList => _myList;
+//
+//   // Adding a movie to the favorites list
+//   void addToList(Movie movie) {
+//     print(_movies);
+//     _myList.add(movie);
+//     notifyListeners();
+//   }
+//
+//   // Removing a movie from the favorites list
+//   void removeFromList(Movie movie) {
+//     _myList.remove(movie);
+//     notifyListeners();
+//   }
+// }

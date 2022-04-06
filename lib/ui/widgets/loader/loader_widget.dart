6855fedd772d944ac68df2fe0f15593f8f25e1cc
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-06 14:50:32 +0530

    common loader widget for loading screen

---
+++ lib/ui/widgets/loader/loader_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/color/colors.dart';
+
+class LoaderWidget extends StatelessWidget {
+  final double radius;
+  const LoaderWidget({Key? key, this.radius = 30}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: radius,
+        height: radius,
+        child: const CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColor.primaryColor2),
+          strokeWidth: 3,
+          backgroundColor: AppColor.yellow2,
+        ),
+      ),
+    );
+  }
+}

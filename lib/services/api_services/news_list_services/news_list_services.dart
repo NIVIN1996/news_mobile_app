c52d4aebd73e9720b7008b3495e4ed7b034531ec
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:24:30 +0530

    api key changed in news_list_services.dart

--- lib/services/api_services/news_list_services/news_list_services.dart
+++ lib/services/api_services/news_list_services/news_list_services.dart
@@ -1,18 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:news_mobile_app/services/api/api_manager.dart';
 import 'package:news_mobile_app/services/end_points/end_points.dart';
 
 import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class NewsListingService {
-  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
+  static const apiKey = 'apiKey=a1ca159c437a41abb943af25efefa5ed';
   static Future<TopHeadlineNewsModel> getTopNewsHeadlines() async {
     return await ApiManager(url: NewsAppUrls.topNewsHeadlines).get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
     });
   }
 
-  static Future<TopHeadlineNewsModel> getCategoryNews(String category) async {
-    return await ApiManager(url: NewsAppUrls.newsHeadlines+"category=$category&"+apiKey).get((p0) {
+  static Future<TopHeadlineNewsModel> getCategoryNews(BuildContext context,String category,int page,int limit) async {
+    return await ApiManager(url: NewsAppUrls.newsHeadlines+"category=$category&"+apiKey+"&page=$page"+"&limit=$limit").get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
     });
   }

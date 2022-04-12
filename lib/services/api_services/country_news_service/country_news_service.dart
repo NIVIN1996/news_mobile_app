b41b1857c2f03259d3b81289503f08d9a779798b
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:10:49 +0530

    news api key changed in country_news_service.dart

--- lib/services/api_services/country_news_service/country_news_service.dart
+++ lib/services/api_services/country_news_service/country_news_service.dart
@@ -1,12 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:news_mobile_app/services/api/api_manager.dart';
 import 'package:news_mobile_app/services/end_points/end_points.dart';
 
 import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class CountryNewsService {
-  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
-  static Future<TopHeadlineNewsModel> getCountryNewsHeadlines(String countryCode) async {
-    return await ApiManager(url: NewsAppUrls.indiaNewsHeadlines+"country=$countryCode&"+apiKey).get((p0) {
+  static const apiKey = 'apiKey=a1ca159c437a41abb943af25efefa5ed';
+  static Future<TopHeadlineNewsModel> getCountryNewsHeadlines(BuildContext context,String countryCode,int page,int limit) async {
+    return await ApiManager(url: NewsAppUrls.indiaNewsHeadlines+"country=$countryCode&"+apiKey+"&page=$page"+"&limit=$limit").get((p0) {
       return TopHeadlineNewsModel.fromJson(p0);
     });
   }

f6b238034fc483fc74caaff249aeccba89705f28
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:28:57 +0530

    apikey changed in search_news_service.dart

--- lib/services/api_services/search_news_service/search_news_service.dart
+++ lib/services/api_services/search_news_service/search_news_service.dart
@@ -5,7 +5,7 @@ import 'package:news_mobile_app/services/end_points/end_points.dart';
 import '../../../models/top_news_headline_model/top_news_headline_model.dart';
 
 class SearchNewsService {
-  static const apiKey = 'apiKey=cbc27393fe50431cb058f81ab4a5931f';
+  static const apiKey = 'apiKey=a1ca159c437a41abb943af25efefa5ed';
   static Future<TopHeadlineNewsModel> getSearchNews(String searchText) async {
 
     return await ApiManager(url: NewsAppUrls.searchNewsHeadlines+"q=$searchText&"+apiKey).get((p0) {

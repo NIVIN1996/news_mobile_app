a34dc278b918af715fd027d010b5aa72ddeecf38
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:30:11 +0530

    email validation regexp changed in validator.dart

--- lib/ui/login_screen/validator.dart
+++ lib/ui/login_screen/validator.dart
@@ -1,3 +1,5 @@
+
+
 class Validator {
   static String? validateName({required String? name}) {
     if (name == null) {
@@ -15,18 +17,16 @@ class Validator {
     if (email == null) {
       return null;
     }
-
     RegExp emailRegExp = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
     if (email.isEmpty) {
       return 'Please enter your email';
     } else if (!emailRegExp.hasMatch(email)) {
       return 'Enter a correct email';
     }
-
     return null;
   }
+
 
   static String? validatePassword({required String? password}) {
     if (password == null) {

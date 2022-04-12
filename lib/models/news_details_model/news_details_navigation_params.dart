431e0c94e892faaf1b37404203d235fffb22922e
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:20:03 +0530

    newsUrl variable added in news_details_navigation_params.dart

--- lib/models/news_details_model/news_details_navigation_params.dart
+++ lib/models/news_details_model/news_details_navigation_params.dart
@@ -6,11 +6,13 @@ class NewsDetailsNavigationParameters {
   String subTitle;
   String publishedAt;
   String pageType;
+  String newsUrl;
   NewsDetailsNavigationParameters(
       {required this.subTitle,
       required this.index,
       required this.pageType,
       required this.title,
+        required this.newsUrl,
       required this.publishedAt,
       required this.imageUrl,
       required this.author});

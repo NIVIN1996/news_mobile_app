b7b22ad4b9905f743ea316fccb37f4b05a58a3d3
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:23:00 +0530

    height set for news_list_item_widget.dart

--- lib/ui/home_screen/home_page_widget/news_list_widget/news_list_item_widget.dart
+++ lib/ui/home_screen/home_page_widget/news_list_widget/news_list_item_widget.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 import 'package:news_mobile_app/models/news_details_model/news_details_navigation_params.dart';
 import 'package:news_mobile_app/ui/navigation/routes.dart';
@@ -20,6 +19,7 @@ class NewsListItemWidget extends StatefulWidget {
   final String author;
   final String subTitle;
   final String pageType;
+  final String newsUrl;
   final DateTime publishedAt;
 
   const NewsListItemWidget({
@@ -30,7 +30,7 @@ class NewsListItemWidget extends StatefulWidget {
     required this.subTitle,
     required this.publishedAt,
     required this.author,
-    required this.pageType,
+    required this.pageType, required this.newsUrl,
   }) : super(key: key);
 
   @override
@@ -42,7 +42,7 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
   DateFormat dateFormat = DateFormat("dd-MM-yyyy");
   @override
   void initState() {
-    Future.delayed(Duration.zero, () {
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
       context.read<ArticleListProvider>().getBookmarkListDb();
     });
     super.initState();
@@ -51,8 +51,6 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
   Widget build(BuildContext context) {
     String date = dateFormat.format(widget.publishedAt);
     String formattedTime = DateFormat('kk:mm a').format(widget.publishedAt);
-    // var myList = context.watch<ArticleListProvider>().myList;
-    var bookmarkItem = context.watch<ArticleListProvider>().bookmarkArticleList;
     var selectedArticle = widget.pageType == "HomeNews"
         ? context.watch<ArticleListProvider>().articleList
         : widget.pageType == "SearchNews"
@@ -72,7 +70,7 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
         context.pushNamed(
           ScreenNames.newsDetailsScreen,
           arguments: NewsDetailsNavigationParameters(
-              index: widget.index,
+              index: widget.index,newsUrl: widget.newsUrl,
               author: widget.author,
               imageUrl: widget.imageUrl,
               publishedAt: date + " " + formattedTime,
@@ -81,119 +79,125 @@ class _NewsListItemWidgetState extends State<NewsListItemWidget> {
               pageType: widget.pageType),
         );
       },
-      child: Card(
-        elevation: 5,
-        margin: EdgeInsets.symmetric(vertical: context.heightPx * 5, horizontal: context.widthPx * 25),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    top: context.widthPx * 15.0, left: context.widthPx * 10.0, right: context.widthPx * 5.0),
-                child: CachedNetworkImage(
-                  imageUrl: widget.imageUrl,
-                  imageBuilder: (context, imageProvider) => Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(context.widthPx * 10),
-                      image: DecorationImage(
-                        image: imageProvider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  placeholder: (context, url) => const ShimmerWidget(
-                    radius: 10,
-                  ),
-                  errorWidget: (context, url, error) => Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(context.widthPx * 20),
-                      image: const DecorationImage(
-                        image: AssetImage('assets/common/no_img.png'),
-                        fit: BoxFit.fitWidth,
-                      ),
-                    ),
-                  ),
-                  height: context.heightPx * 100,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: context.widthPx * 7),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: context.heightPx * 0.0, bottom: context.heightPx * 0.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              widget.author,
-                              textAlign: TextAlign.start,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextFontStyle.med(size: context.textPx * 16),
-                            ),
-                          ),
-                          IconButton(
-                            padding: const EdgeInsets.all(0),
-                            icon: Icon(
-                              articleDb.containsKey(article.title) ? Icons.bookmark : Icons.bookmark_outline,
-                                color: AppColor.yellow2, size: context.widthPx * 20),
-                            onPressed: () {
-
-                              if (!articleDb.containsKey(article.title)) {
-                                context.read<ArticleListProvider>().addBookmark(article);
-                                AppSnackBar.showSnackBarWithText(context: context, text: "Added To Bookmark");
-                              } else {
-                                context.read<ArticleListProvider>().removeFromList(article);
-                                AppSnackBar.showSnackBarWithText(context: context, text: "Removed From Bookmark");
-                              }
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    Text(
-                      widget.title,
-                      textAlign: TextAlign.start,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextFontStyle.regular(size: context.textPx * 14),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: context.heightPx * 8.0,
+      child: SizedBox(
+        height: context.heightPx * 155,
+        child: Card(
+          elevation: 5,
+          margin: EdgeInsets.symmetric(vertical: context.heightPx * 5, horizontal: context.widthPx * 25),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: context.widthPx * 15.0, left: context.widthPx * 10.0, right: context.widthPx * 5.0),
+                  child: CachedNetworkImage(
+                    imageUrl: widget.imageUrl,
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(context.widthPx * 10),
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
                         ),
-                        child: RichText(
-                          text: TextSpan(
-                            children: [
-                              WidgetSpan(
-                                child: Padding(
-                                  padding: EdgeInsets.only(right: context.widthPx * 5.0),
-                                  child: Icon(Icons.access_time, size: context.widthPx * 18),
-                                ),
-                              ),
-                              TextSpan(
-                                text: date + " " + formattedTime,
-                                style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
-                              ),
-                            ],
-                          ),
-                        )),
-                  ],
+                      ),
+                    ),
+                    placeholder: (context, url) => const ShimmerWidget(
+                      radius: 10,
+                    ),
+                    errorWidget: (context, url, error) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(context.widthPx * 20),
+                        image: const DecorationImage(
+                          image: AssetImage('assets/common/no_img.png'),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                    height: context.heightPx * 100,
+                  ),
                 ),
               ),
-            )
-          ],
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: context.widthPx * 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: context.heightPx * 0.0, bottom: context.heightPx * 0.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                widget.author,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextFontStyle.med(size: context.textPx * 16),
+                              ),
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.all(0),
+                              icon: Icon(
+                                articleDb.containsKey(article.title) ? Icons.bookmark : Icons.bookmark_outline,
+                                  color: AppColor.yellow2, size: context.widthPx * 20),
+                              onPressed: () {
+
+                                if (!articleDb.containsKey(article.title)) {
+                                  context.read<ArticleListProvider>().addBookmark(article);
+                                  AppSnackBar.showSnackBarWithText(context: context, text: "Added To Bookmark");
+                                } else {
+                                  context.read<ArticleListProvider>().removeFromList(article);
+                                  AppSnackBar.showSnackBarWithText(context: context, text: "Removed From Bookmark");
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: context.heightPx * 60,
+                        child: Text(
+                          widget.title,
+                          textAlign: TextAlign.start,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextFontStyle.regular(size: context.textPx * 14),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: context.heightPx * 8.0,
+                          ),
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                WidgetSpan(
+                                  child: Padding(
+                                    padding: EdgeInsets.only(right: context.widthPx * 5.0),
+                                    child: Icon(Icons.access_time, size: context.widthPx * 18),
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: date + " " + formattedTime,
+                                  style: TextFontStyle.med(color: AppColor.grey5, size: context.textPx * 14),
+                                ),
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ):

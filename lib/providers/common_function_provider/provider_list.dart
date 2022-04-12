acb44aa87f5f7500d44107c907f90ecd4f461d88
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:26:54 +0530

    removed unwanted comments from provider_list.dart

--- lib/providers/common_function_provider/provider_list.dart
+++ lib/providers/common_function_provider/provider_list.dart
@@ -2,11 +2,10 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import '../news_list_provider/news_list_provider.dart';
-import '../sample_provider.dart';
+
 
 class ProviderRegistrar {
   static List<SingleChildWidget> providers = [
-    // ChangeNotifierProvider(create: (_) => MovieProvider()),
     ChangeNotifierProvider(create: (_) => ArticleListProvider()),
   ];
 }

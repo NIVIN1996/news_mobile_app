39539051316ee3aa4ea57b5ddee6aab08ba6479a
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-12 10:18:30 +0530

    unwanted imports removed and pull to refresh added

--- lib/ui/news_category_list_screen/news_category_list_screen.dart
+++ lib/ui/news_category_list_screen/news_category_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_mobile_app/utils/color/colors.dart';
 import 'package:news_mobile_app/utils/responsive_config/responsive_config.dart';
 import 'package:provider/provider.dart';
 
@@ -6,10 +7,11 @@ import '../../models/category_model/category_model_navigation_params.dart';
 
 import '../../providers/news_list_provider/news_list_provider.dart';
 import '../../utils/static/enums.dart';
-import '../../utils/text_style/text_style.dart';
 import '../home_screen/home_page_widget/news_list_widget/news_list_item_widget.dart';
 
 import '../widgets/app_bar_widget/common_app_bar_widget.dart';
+import '../widgets/common_text_widget/highlight_text_widget.dart';
+import '../widgets/loader/pagination_loader_widget.dart';
 import '../widgets/shimmer_widget/shimmer_widget.dart';
 
 class NewsCategoryListScreen extends StatefulWidget {
@@ -24,12 +26,44 @@ class NewsCategoryListScreen extends StatefulWidget {
 }
 
 class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool onNotification(ScrollNotification notification) {
+    ArticleListProvider dataProvider = Provider.of<ArticleListProvider>(context, listen: false);
+
+    if (notification is ScrollUpdateNotification && notification.metrics.axisDirection == AxisDirection.down) {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent &&
+          dataProvider.categoryNewsModel != null) {
+        int lastPage = 4;
+        int currentPage = dataProvider.currentPage;
+        if (currentPage < lastPage) {
+          dataProvider.setPageCount(currentPage += 1);
+          dataProvider.initCategoryList(context, widget.navigationParameters.category);
+        }
+      }
+    }
+    return true;
+  }
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () {
-      context.read<ArticleListProvider>().getCategoryArticle(widget.navigationParameters.category);
+
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      context
+          .read<ArticleListProvider>()
+          .initCategoryList(context, widget.navigationParameters.category, initial: true);
     });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _refresh() {
+    return context.read<ArticleListProvider>().getCategoryArticle(context, widget.navigationParameters.category);
   }
 
   @override
@@ -44,64 +78,70 @@ class _NewsCategoryListScreenState extends State<NewsCategoryListScreen> {
         notificationPress: () {},
         appBar: AppBar(),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: context.widthPx * 25.0, vertical: context.heightPx * 10),
-              child: Text(
-                "Highlights",
-                textAlign: TextAlign.start,
-                style: TextFontStyle.semiBold(size: context.textPx * 20),
-              ),
-            ),
-            Consumer<ArticleListProvider>(builder: (context, provider, _) {
-              if (provider.articleInitStatus == ApiStatus.loading) {
-                return Column(
-                  children: [
-                    for (int i = 0; i < 4; i++)
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: context.widthPx * 25.0, vertical: context.heightPx * 5),
-                        child: ShimmerWidget(
-                          height: context.heightPx * 140,
-                          width: double.infinity,
-                          radius: 10,
-                        ),
-                      )
-                  ],
-                );
-              }
-              return ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  itemCount:
-                      provider.articleInitStatus == ApiStatus.success ? provider.categoryArticleList.length : 5,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return provider.articleInitStatus == ApiStatus.success
-                        ? NewsListItemWidget(
-                            title: provider.categoryArticleList[index].title,
-                            imageUrl: provider.categoryArticleList[index].urlToImage,
-                            publishedAt: provider.categoryArticleList[index].publishedAt!,
-                            subTitle: provider.categoryArticleList[index].content,
-                            index: index,
-                            author: provider.categoryArticleList[index].source.name,
-                            pageType: 'CategoryNews',
-                          )
-                        : Padding(
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        color: AppColor.yellow2,
+        child: NotificationListener(
+          onNotification: onNotification,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HighLightTextWidget(),
+                Consumer<ArticleListProvider>(builder: (context, provider, _) {
+                  if (provider.articleInitStatus == ApiStatus.loading) {
+                    return  Column(
+                      children: [
+                        for (int i = 0; i < 6; i++)
+                          Padding(
                             padding: EdgeInsets.symmetric(
                                 horizontal: context.widthPx * 25.0, vertical: context.heightPx * 5),
                             child: ShimmerWidget(
                               height: context.heightPx * 140,
-                              width: context.widthPx * 100,
+                              width: double.infinity,
                               radius: 10,
                             ),
-                          );
-                  });
-            }),
-          ],
+                          )
+                      ],
+                    );
+                  }
+                  return Column(
+                    children: [
+                      ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          scrollDirection: Axis.vertical,
+                          itemCount: provider.articleInitStatus == ApiStatus.success ? provider.categoryData.length : 5,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return provider.articleInitStatus == ApiStatus.success
+                                ? NewsListItemWidget(
+                                    title: provider.categoryData[index].title,
+                                    imageUrl: provider.categoryData[index].urlToImage,
+                                    publishedAt: provider.categoryData[index].publishedAt!,
+                                    subTitle: provider.categoryData[index].content,
+                                    index: index,
+                                    author: provider.categoryData[index].source.name,
+                                    pageType: 'CategoryNews', newsUrl: provider.categoryData[index].url,
+                                  )
+                                : Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: context.widthPx * 25.0, vertical: context.heightPx * 5),
+                                    child: ShimmerWidget(
+                                      height: context.heightPx * 140,
+                                      width: context.widthPx * 100,
+                                      radius: 10,
+                                    ),
+                                  );
+                          }),
+                      pageLoaderWidget(context, context.watch<ArticleListProvider>().isLoading),
+
+                    ],
+                  );
+                }),
+              ],
+            ),
+          ),
         ),
       ),
     );
